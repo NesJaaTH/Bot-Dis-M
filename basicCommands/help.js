526d7b02133e9b2057f54abdc1d6af5cd671c11b
NesJaaTH <nestv001@gmail.com>
2024-06-19 02:20:05 +0700

    d

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -17,7 +17,7 @@ module.exports = {
         url: 'https://discord.gg/FUEHs7RCqz'
     })
      
-      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [Skip-Shop]()**\n\n__**COMMANDS :**__ `)
+      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [Skip-Shop](https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A)**\n\n__**COMMANDS :**__ `)
       .addFields(
       
         {
@@ -56,22 +56,22 @@ module.exports = {
           inline: true,
         }
       )
-      // .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      // .setImage(`https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=6641e8b7&is=66409737&hm=96f69ba8bcde4ca44bf8713e22001e0bafad0335cfd1f57c439a34b4762f0e03&`);
+      .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+      .setImage(`https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=6641e8b7&is=66409737&hm=96f69ba8bcde4ca44bf8713e22001e0bafad0335cfd1f57c439a34b4762f0e03&`);
 
     const button1 = new ButtonBuilder()
       .setLabel('YouTube')
-      // .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+      .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
       .setStyle(ButtonStyle.Link);
 
     const button2 = new ButtonBuilder()
       .setLabel('Discord')
-      // .setURL('https://discord.gg/xQF9f9yUEM')
+      .setURL('https://discord.gg/xQF9f9yUEM')
       .setStyle(ButtonStyle.Link);
 
     const button3 = new ButtonBuilder()
       .setLabel('Code')
-      // .setURL('https://github.com/GlaceYT')
+      .setURL('https://github.com/GlaceYT')
       .setStyle(ButtonStyle.Link);
       
     const row = new ActionRowBuilder()
